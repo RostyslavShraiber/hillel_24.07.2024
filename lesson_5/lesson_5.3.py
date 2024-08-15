1d347338d2f436a9d4446a98b94471a60d59ae5a
RostyslavShraiber <rostiksrajber@gmail.com>
2024-08-15 20:51:06 +0300

    fix lesson_4.2, lesson_5.3

--- lesson_5/lesson_5.3.py
+++ lesson_5/lesson_5.3.py
@@ -2,20 +2,22 @@ import string
 
 #отримуємо значення рядка
 input_string = input("Введіть рядок: ")
-
-#перетворюємо першу літеру кожного слова на велику
-input_string = input_string.title()
-
-#визначаємо значення очищеного рядка від символів та пробілів (за замовчуванням)
 clean_string = ""
 
 #визначаємо значення очищеного рядка від символів та пробілів на основі отриманого рядка з поля вводу
 for char in input_string:
-    if char not in string.punctuation and char != " ":
+    if char not in string.punctuation:
         clean_string += char
 
+#перетворення рядка на список зі словами
+array_string = clean_string.split(' ')
+hashtag = ""
+
+for word in array_string:
+    hashtag += word.capitalize()
+
 #створюємо хештег (додаємо # на початок)
-hashtag = "#" + clean_string
+hashtag = "#" + hashtag
 
 #проводимо перевірку на довжину хештегу (довжина не повинна перевищувати 140 символів) та друкуємо його
 if len(hashtag) <= 140:
